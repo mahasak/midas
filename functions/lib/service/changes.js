d74c664d307c73b256f94848ca1d9a6453b9e890
Max Pijittum <mahasak@gmail.com>
2022-11-22 02:13:57 +0800

    remove redundant invoice edit function

--- functions/lib/service/changes.js
+++ functions/lib/service/changes.js
@@ -1,39 +1,38 @@
 const functions = require('firebase-functions');
 const fetch = require('node-fetch');
 const { sendOrderCTA, sendTextMessage } = require('./messenger')
-
+const { getSessionData, getOrderData } = require('./session')
 const PAGE_ID = functions.config().facebook.page_id;
 const ACCESS_TOKEN = functions.config().facebook.access_token;
 
-const { invoiceAccessInvoiceEdit2 } = require('../service/invoiceAccessInvoiceEdit')
-const {debug,logger} = require('../logger')
+const { invoiceAccessInvoiceEdit } = require('../service/invoiceAccessInvoiceEdit')
+const { debug, logger } = require('../logger')
 
 exports.receivedChanges = async (change) => {
     // process payment signal / bank slips
     if (change.field === 'invoice_access_invoice_draft_change') {
-        
-        console.log(change)
-        console.log('####################################################')
-        await console.log("invoice draft change detected")
-        console.log('####################################################')
-        var startTime = performance.now()
+        debug('Incoming changeset for invoice_access_invoice_draft_change', change)
+        debug('Incoming changeset for invoice_access_invoice_draft_change', JSON.stringify(change))
+        logger.info(`[webhook-invoice-draft-change-handler] Changeset for ${change.value.invoice_id}`)
+        const startTime = performance.now()
 
         const res = await fetch(`https://graph.facebook.com/v14.0/${PAGE_ID}/invoice_access_invoice_details?invoice_id=${change.value.invoice_id}&access_token=${ACCESS_TOKEN}`, {
             method: 'GET',
         });
 
         const data = await res.json();
-        console.log(data)
+        debug('order details', data)
 
         if (res.ok) {
+            
             const invoiceIdFromAPI = data.data[0].invoice_id
             let additionalAmounts = data.data[0].additional_amounts
+            logger.info(`[webhook-changes-handler] Successfully fetch order with ID ${invoiceIdFromAPI}`)
 
             // address changes
             let newShippingAddress = null
             if (change.value.updates.shipping_address !== undefined) {
-                console.log('----------- shipping address changes ----------')
-                console.log(change.value.updates.shipping_address)
+                debug('shipping address changes', change.value.updates.shipping_address)
                 newShippingAddress = change.value.updates.shipping_address.new_value
                 // generate random shipping fee
                 const shippingFeeFactor = Math.floor((Math.random() * 10) + 1);
@@ -47,14 +46,11 @@ exports.receivedChanges = async (change) => {
                     }
                 ]
             }
-        
-
 
             let paid_amount = 0
             if (data.data[0].payments !== undefined) {
                 for (const payment of data.data[0].payments) {
-                    console.log(payment)
-                    console.log(payment.metadata)
+                    debug('payment changes', payment)
                     if (payment.metadata.bank_slip.validation_status === "verified") {
                         paid_amount += parseFloat(payment.payment_amount.amount)
                     }
@@ -66,22 +62,23 @@ exports.receivedChanges = async (change) => {
                 }
             }
 
-
-
-            console.log(`Total Paid: ${paid_amount}`)
-
-            console.log("Successfully fetch order with ID %s", invoiceIdFromAPI)
-            const result = await invoiceAccessInvoiceEdit2(
+            logger.info(`[webhook-invoice-draft-changes-handler] Invoice ${invoiceIdFromAPI} total paid: ${paid_amount}`)
+            const orderDetail = await getOrderData(data.data[0].invoice_id)
+            const result = await invoiceAccessInvoiceEdit(
+                orderDetail.psid,
                 data.data[0].external_invoice_id,
                 data.data[0].invoice_id,
                 data.data[0].buyer_notes,
-                paid_amount,
+                {
+                    "amount": `${paid_amount}`,
+                    "currency": "THB"
+                },
                 additionalAmounts,
                 data.data[0].product_items,
                 newShippingAddress
             )
-            var endTime = performance.now()
-            console.log(`Call to invoice_access_invoice_edit took ${(endTime - startTime)/1000} seconds`)
+            const endTime = performance.now()
+            logger.info(`[webhook-invoice-draft-changes-handler] Call to invoice_access_invoice_edit took ${(endTime - startTime) / 1000} seconds`)
             return {
                 invoiceId: invoiceIdFromAPI,
             }
@@ -90,9 +87,7 @@ exports.receivedChanges = async (change) => {
 
     // process order updates
     if (change.field === 'invoice_access_invoice_change') {
-        console.log('####################################################')
-        await console.log("invoice change detected")
-        console.log('####################################################')
-
+        debug('Incoming changeset for invoice_access_invoice_change', change)
+        logger.info(`[webhook-invoice-change-handler] Changeset for ${change.value.invoice_id}`)
     }
 }
--- functions/lib/service/invoiceAccessInvoiceEdit.js
+++ functions/lib/service/invoiceAccessInvoiceEdit.js
@@ -52,9 +52,11 @@ exports.invoiceAccessInvoiceEdit = async (buyerId, orderId, invoiceId, note, pai
         note += 'Order summary\n'
         note += '--------------------\n'
         let total = 0
-        for (const productItem of productItems) {
-            note += `${productItem.name} x ${productItem.quantity} = ${productItem.currency_amount.amount * productItem.quantity} ${productItem.currency_amount.currency}\n`
-            total += (productItem.currency_amount.amount * productItem.quantity)
+        if (productItems !== null && productItems !== undefined)  {
+            for (const productItem of productItems) {
+                note += `${productItem.name} x ${productItem.quantity} = ${productItem.currency_amount.amount * productItem.quantity} ${productItem.currency_amount.currency}\n`
+                total += (productItem.currency_amount.amount * productItem.quantity)
+            }
         }
         note += '--------------------\n'
         for (const additionalAmount of updateAdditionalAmount) {
@@ -75,93 +77,6 @@ exports.invoiceAccessInvoiceEdit = async (buyerId, orderId, invoiceId, note, pai
     } else {
         console.log(data);
         console.log("Failed to uptdate order for recipient %s", buyerId)
-        return undefined
-    }
-}
-
-exports.invoiceAccessInvoiceEdit2 = async (orderId, invoiceId, note, paidAmount, additionalAmounts, productItems, shippingAddress) => {
-    const payload = {
-        "invoice_id": `${invoiceId}`,
-        "notes": `${note}`,
-        "paid_amount": {
-            "amount": `${paidAmount}`,
-            "currency": "THB"
-        },
-
-        "product_items": productItems
-    }
-    const updateAdditionalAmount = []
-    if (additionalAmounts !== null) {
-        if (additionalAmounts !== null) {
-            for (const additionalAmount of additionalAmounts) {
-                updateAdditionalAmount.push({
-                    label: additionalAmount.label,
-                    currency_amount: {
-                        amount: parseInt(additionalAmount.currency_amount.amount) / 100,
-                        currency: additionalAmount.currency_amount.currency
-                    }
-                })
-            }
-        }
-        payload["additional_amounts"] = updateAdditionalAmount
-    }
-    if (shippingAddress !== null) {
-        payload['shipping_address'] = shippingAddress
-    }
-    console.log("UPDATE PAYLOAD")
-    console.log(payload)
-
-    const res = await fetch('https://graph.facebook.com/v14.0/' + PAGE_ID + '/invoice_access_invoice_edit?access_token=' + ACCESS_TOKEN, {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers: { 'Content-Type': 'application/json' }
-
-    });
-
-    const data = await res.json();
-    console.log(`---------- Invoice Edit Response -----------`)
-    console.log(data)
-
-    if (res.ok) {
-        
-        const invoiceId = data.invoice_id
-        const orderDetail = await getOrderData(invoiceId)
-        const buyerId = orderDetail.psid
-        const orderId = orderDetail.orderId
-
-        console.log("Successfully update order with ID %s ", invoiceId)
-
-        let note = `Order #${orderId} from NativeBear (Edit2)🐻\n\n`
-        note += 'Order summary\n'
-        note += '--------------------\n'
-        let total = 0
-        if (productItems !== null && productItems !== undefined)  {
-            for (const productItem of productItems) {
-                note += `${productItem.name} x ${productItem.quantity} = ${productItem.currency_amount.amount * productItem.quantity} ${productItem.currency_amount.currency}\n`
-                total += (productItem.currency_amount.amount * productItem.quantity)
-            }
-        }
-        note += '--------------------\n'
-
-        for (const additionalAmount of updateAdditionalAmount) {
-            note += `${additionalAmount.label} = ${additionalAmount.currency_amount.amount} ${additionalAmount.currency_amount.currency}\n`
-            total += parseInt(additionalAmount.currency_amount.amount)
-        }
-        note += '--------------------\n'
-
-        note += `Paid amount: ${paidAmount} THB\n`
-        note += `Total amount: ${total} THB`
-
-        console.log(buyerId);
-        await sendOrderCTA(buyerId, note, invoiceId)
-        return {
-            invoiceId: invoiceId,
-            orderId: orderId
-        }
-        
-    } else {
-        console.log("Failed to uptdate order for recipient")
-        console.log(data)
         return undefined
     }
 }
